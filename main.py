95693537be50105db8c21dbc2365fd0f267c57a0
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2022-10-13 15:48:29 -0400

    Put Delete finished

--- main.py
+++ main.py
@@ -61,10 +61,12 @@ def get_user_info(user_id: int = 0) -> FullUserProfile:
     return FullUserProfile(**full_user_profile)
 
 
-def create_user(full_profile_info: FullUserProfile) -> int:
+def create_update_user(full_profile_info: FullUserProfile, new_user_id: Optional[int] = None) -> int:
     global profile_infos
     global users_content
-    new_user_id = len(profile_infos)
+
+    if new_user_id is None:
+        new_user_id = len(profile_infos)
     liked_posts = full_profile_info.liked_posts
     short_description = full_profile_info.short_description
     long_bio = full_profile_info.long_bio
@@ -94,6 +96,14 @@ def get_all_users_with_pagination(start: int, limit: int) -> (list[FullUserProfi
     return list_of_users, total
 
 
+def delete_user(user_id: int) -> None:
+    global profile_infos
+    global users_content
+
+    del profile_infos[user_id]
+    del users_content[user_id]
+
+
 @app.get("/user/me", response_model=FullUserProfile)
 def test_endpoint():
     return get_user_info()
@@ -105,6 +115,17 @@ def get_user_by_id(user_id: int):
     return full_user_profile
 
 
+@app.put("/users/{user_id}")
+def update_user(user_id: int, full_profile_info: FullUserProfile):
+    create_update_user(full_profile_info, user_id)
+    return None
+
+
+@app.delete("/users/{user_id}")
+def remove_user(user_id: int):
+    delete_user(user_id)
+
+
 @app.get("/users", response_model=MultipleUsersResponse)
 def get_all_users_paginated(start: int = 0, limit: int = 20):
     users, total = get_all_users_with_pagination(start, limit)
@@ -114,6 +135,6 @@ def get_all_users_paginated(start: int = 0, limit: int = 20):
 
 @app.post("/users", response_model=CreateUserResponse)
 def add_user(full_profile_info: FullUserProfile):
-    user_id = create_user(full_profile_info)
+    user_id = create_update_user(full_profile_info)
     created_user = CreateUserResponse(user_id=user_id)
     return created_user
