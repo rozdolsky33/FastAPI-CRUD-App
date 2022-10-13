7cf42508945c65efd9c83a33160da24055673b48
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2022-10-13 17:36:48 -0400

    refactoring directory structure formatting

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
-from app.routes.user import user_router
 
-app = FastAPI()
-app.include_router(user_router)
+from app.routes.user import create_user_router
+
+
+def create_application() -> FastAPI:
+    user_router = create_user_router()
+
+    app = FastAPI()
+    app.include_router(user_router)
+    return app
+
+
+app = create_application()
